437660daf1f228549c39f99c5f869b820db007cd
Jean-Philippe Gauthier <Jean-Philippe.Gauthier@canada.ca>
2014-12-12 18:45:44 +0000

    @#! AIX does not have timersub macro (pasted from /sys/time.h)

--- src/eerUtils.h
+++ src/eerUtils.h
@@ -49,6 +49,19 @@
 #include <math.h>
 #include <malloc.h>
 
+// $@#! AIX does not have timersub macro (pasted from /sys/time.h)
+#ifndef timersub
+#define timersub(a, b, result)                       \
+  do {                                 \
+    (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;              \
+    (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;              \
+    if ((result)->tv_usec < 0) {                   \
+      --(result)->tv_sec;                       \
+      (result)->tv_usec += 1000000;                   \
+    }                               \
+  } while (0)
+#endif
+
 /*System related constants and functions*/
 #define SYS_BIG_ENDIAN     0
 #define SYS_LITTLE_ENDIAN  1
