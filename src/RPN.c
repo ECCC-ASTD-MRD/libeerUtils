0b139f1f4c231fb18ab5e0d3f888f86577e7af1a
Eric Legault-Ouellet <eric.legault-ouellet@canada.ca>
2017-05-26 15:19:39 -0400

    Since fstinl returns an error in case we bust the limit, the limit has been heightened and that specific error is now catched

--- src/RPN.c
+++ src/RPN.c
@@ -657,8 +657,8 @@ int RPN_FieldTile(int FID,TDef *Def,TRPNHeader *Head,TGeoRef *GRef,TZRef *ZRef,i
  *----------------------------------------------------------------------------
  */
 int RPN_GetAllFields(int FID,int DateV,char *Etiket,int Ip1,int Ip2,int Ip3,char *Typvar,char *Nomvar,int **Arr,int *Size) {
-    int ni,nj,nk;
-    int n=128;
+    int ni,nj,nk,err;
+    int n=32768; // ~128kiB
     int *arr=NULL,s;
 
     *Arr    = NULL;
@@ -666,8 +666,8 @@ int RPN_GetAllFields(int FID,int DateV,char *Etiket,int Ip1,int Ip2,int Ip3,char
 
     do {
         APP_MEM_ASRT(arr,realloc(arr,(n*=2)*sizeof(*arr)));
-        if( cs_fstinl(FID,&ni,&nj,&nk,DateV,Etiket,Ip1,Ip2,Ip3,Typvar,Nomvar,arr,&s,n) ) {
-            App_Log(ERROR,"(RPN_GetAllFields) Couldn't get list of fields (cs_fstinl)\n");
+        if( (err=cs_fstinl(FID,&ni,&nj,&nk,DateV,Etiket,Ip1,Ip2,Ip3,Typvar,Nomvar,arr,&s,n)) && err!=-4762 ) {
+            App_Log(ERROR,"(RPN_GetAllFields) Couldn't get list of fields (cs_fstinl) code=%d\n",s,n,err);
             APP_FREE(arr);
             return(APP_ERR);
         }
