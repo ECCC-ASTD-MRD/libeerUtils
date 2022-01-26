ECCC-ASTD-MRD/libeerUtils

commit 870678b8ef1f317a51e09610fa40097c07937141

Fixed some warnings

util/Dict.c
@@ -89,11 +89,11 @@ int main(int argc, char *argv[]) {
       type=strdup("");
       search=DICT_GLOB;
    } else {
-      if (var==(void*)APP_FLAG || (var && strncmp(var,"all",3)==0) || !var&&(ip1>=0||ip3>=0||state||origin)) {
+      if ((var==(void*)APP_FLAG) || (var && strncmp(var,"all",3)==0) || !(var&&(ip1>=0||ip3>=0||state||origin))) {
          var=strdup("");
          search=DICT_GLOB;
       }
-      if (type==(void*)APP_FLAG || (type && strncmp(type,"all",3)==0)) {
+      if ((type==(void*)APP_FLAG) || (type && strncmp(type,"all",3)==0)) {
          type=strdup("");
          search=DICT_GLOB;
       }
@@ -312,7 +312,7 @@ int Dict_CheckRPN(char **RPNFile,int CheckOPS) {
    TRPNHeader headtable[DICT_MAXFLD],*head;
    TDictVar  *var;
    TList     *unknown,*known;
-   int        fid,key,ni,nj,nk,n,nb_unknown=0,nb_known=0,nidx,idxs[DICT_MAXREF],dateo=0;
+   int        fid,ni,nj,nk,n,nb_unknown=0,nb_known=0,nidx,idxs[DICT_MAXREF],dateo=0;
    double     nhour;
    char       nvar[5],*etiket;
 
@@ -489,7 +489,7 @@ int Dict_CheckRPN(char **RPNFile,int CheckOPS) {
                nhtic=nhtac=nhtoc=nhtuc=0;
                nh++;
                for(z=0;z<headnb;z++) {
-                  if (href[h].IP1=headtable[z].IP1 && href[h].IP2==headtable[z].IP2 && href[h].IP3==headtable[z].IP3) {
+                  if ((href[h].IP1==headtable[z].IP1) && (href[h].IP2==headtable[z].IP2) && (href[h].IP3==headtable[z].IP3)) {
                      if (!strcmp(">>  ",headtable[z].NOMVAR)) nhtic=1;
                      if (!strcmp("^^  ",headtable[z].NOMVAR)) nhtac=1;
                      if (!strcmp("!!  ",headtable[z].NOMVAR)) nhtoc=1;

.gitlab-ci.yml
@@ -16,7 +16,10 @@ build:intel-19.0.3:
       - master
       - dev
    script:
-      - source ci-env/latest/ubuntu-18.04-skylake-64/intel-19.0.3.199.sh
+      - source ci-env/latest/ubuntu-18.04-amd-64/intel-19.0.3.199.sh
+      - export SSM_DEV=/users/dor/afsr/005/links/storage/ssm
+      - export CMD_EXT_PATH=/ssm/net/cmds/ext/20210301
+      - . ssmuse-sh -x $CMD_EXT_PATH
       - mkdir build_intel-19.0.3
       - cd build_intel-19.0.3
       - cmake ../ 
@@ -33,6 +36,9 @@ build:gnu-7.4.0:
       - dev
    script:
       - source ci-env/latest/ubuntu-18.04-amd-64/gnu-7.4.0.sh
+      - export SSM_DEV=/users/dor/afsr/005/links/storage/ssm
+      - export CMD_EXT_PATH=/ssm/net/cmds/ext/20210301
+      - . ssmuse-sh -x $CMD_EXT_PATH
       - mkdir build_gnu-7.4.0
       - cd build_gnu-7.4.0
       - cmake ../ 

src/ZRef.c
@@ -612,9 +612,9 @@ double ZRef_K2Pressure(TZRef* restrict const ZRef,double P0,int K) {
  */
 int ZRef_KCube2Pressure(TZRef* restrict const ZRef,float *P0,int NIJ,int Log,float *Pres) {
 
-   unsigned int k,idxk=0,ij;
-   int          *ips;
-   float        pref,ptop,*p0;
+   int   k,idxk=0,ij;
+   int   *ips;
+   float pref,ptop,*p0;
 
    if (!P0 && ZRef->Type!=LVL_PRES) {
       App_Log(ERROR,"%s: Surface pressure is required\n",__func__);

util/CodeInfo.c
@@ -57,7 +57,7 @@
 int Codec(char *Pool,char *FST,char *Var,int Code) {
 
  #ifdef HAVE_RMN
-   char       buf[APP_BUFMAX*8],*c;
+   char       buf[APP_BUFMAX*8];
    int        fld[APP_BUFMAX*8],len,err,fstid,i;
    FILE      *fid=NULL;
    TRPNHeader h;
@@ -69,30 +69,31 @@ int Codec(char *Pool,char *FST,char *Var,int Code) {
       }
 
       // Get the pool line
-      c=fgets(buf,APP_BUFMAX*8,fid);
+      if (fgets(buf,APP_BUFMAX*8,fid)) {
 
-      // Get rid of trailing spaces
-      strtrim(buf,' ');
-      len=strlen(buf);
+         // Get rid of trailing spaces
+         strtrim(buf,' ');
+         len=strlen(buf);
 
-      // Get rid of trailing \n
-      if (buf[len-1]=='\n') len--;
-      
-      App_Log(INFO,"Encoding %i character\n",len);
-      for(i=0;i<len;i++) {
-         fld[i]=buf[i];
-      }
-      
-      if ((fstid=cs_fstouv(FST,"STD+RND+R/W"))<0) {
-         App_Log(ERROR,"Problems opening output file %s\n",FST);
-         return(0);
-      }
-      App_Log(INFO,"Encoding into %s\n",FST);
+         // Get rid of trailing \n
+         if (buf[len-1]=='\n') len--;
+         
+         App_Log(INFO,"Encoding %i character\n",len);
+         for(i=0;i<len;i++) {
+            fld[i]=buf[i];
+         }
+         
+         if ((fstid=cs_fstouv(FST,"STD+RND+R/W"))<0) {
+            App_Log(ERROR,"Problems opening output file %s\n",FST);
+            return(0);
+         }
+         App_Log(INFO,"Encoding into %s\n",FST);
 
-      err=cs_fstecr(fld,-8,fstid,0,0,0,len,1,1,0,0,0,"X",Var,"DESCRIPTION","X",0,0,0,0,2,TRUE);
-      if (err<0) {
-         App_Log(ERROR,"Could not write encoded pool record\n");
-         return(0);
+         err=cs_fstecr(fld,-8,fstid,0,0,0,len,1,1,0,0,0,"X",Var,"DESCRIPTION","X",0,0,0,0,2,TRUE);
+         if (err<0) {
+            App_Log(ERROR,"Could not write encoded pool record\n");
+            return(0);
+         }
       }
    } else {
       if ((fstid=cs_fstouv(FST,"STD+RND+R/O"))<0) {

commit f975e4c9514192cd35d674c8c4af9bfa10bc16cf

Fixed bug introduced in commit 870678b8 that reversed the test logic

util/Dict.c
@@ -89,7 +89,7 @@ int main(int argc, char *argv[]) {
       type=strdup("");
       search=DICT_GLOB;
    } else {
-      if ((var==(void*)APP_FLAG) || (var && strncmp(var,"all",3)==0) || !(var&&(ip1>=0||ip3>=0||state||origin))) {
+      if ((var==(void*)APP_FLAG) || (var && strncmp(var,"all",3)==0) || !var&&(ip1>=0||ip3>=0||state||origin)) {
          var=strdup("");
          search=DICT_GLOB;
       }