2f647a507afc0803e180d5efd68546e5482015ef
Eric Legault-Ouellet <eric.legault-ouellet@ec.gc.ca>
2025-01-08 14:18:28 -0500

    [Dict] Fixed an overlap where DICT_INTEGER and DICT_INCOMPLETE had the same value

--- src/Dict.h
+++ src/Dict.h
@@ -47,10 +47,11 @@
 #define DICT_FUTURE     0x08
 #define DICT_INCOMPLETE 0x10
 
-#define DICT_INTEGER  0x10
-#define DICT_REAL     0x20
-#define DICT_LOGICAL  0x40
-#define DICT_CODE     0x80
+#define DICT_INTEGER    0x100
+#define DICT_REAL       0x200
+#define DICT_LOGICAL    0x400
+#define DICT_CODE       0x800
+#define DICT_TYPE       0xF00
 
 #define DICT_EXACT    0
 #define DICT_GLOB     1
